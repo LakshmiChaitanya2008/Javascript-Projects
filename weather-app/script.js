2ebe89bb5073f57f21d92d6a563ed398e987cb2b
LakshmiChaitanya2008 <72391391+LakshmiChaitanya2008@users.noreply.github.com>
2021-08-03 20:01:38 +0530

    Update script.js

--- weather-app/script.js
+++ weather-app/script.js
@@ -6,15 +6,19 @@ const labelWeather = document.querySelector(".weather");
 const labelMinMax = document.querySelector(".hi-low");
 
 const key = "d7681e7535a4b5a75b255b1ab312d798";
-const keyLocation = 'pk.de9b2c7acf1405437d83751bf89e297e';
+const keyLocation = "pk.de9b2c7acf1405437d83751bf89e297e";
 
 // Getting City With lat,lng
 const getCity = function (lat, lng) {
-  fetch(`https://us1.locationiq.com/v1/reverse.php?key=${keyLocation}&lat=${lat}&lon=${lng}&zoom=10&format=json`)
-  .then(res => res.json())
-  .then(res => {
-    getResults(res.address.city)
-  })
+  fetch(
+    `https://api.geoapify.com/v1/geocode/reverse?lat=${lat}&lon=${lng}&apiKey=573bb7557b384d8897f6bcfa8f2cf803`
+  )
+    .then((res) => res.json())
+    .then((res) => {
+      const city = res.features[0].properties.city;
+      getResults(city);
+    })
+    .catch((error) => alert(error));
 };
 
 // Getting User Location
@@ -29,11 +33,14 @@ getLocation();
 
 // Rendering Results from getResults
 const renderResults = function (data) {
-  labelCity.textContent = `${data.name} ${data.sys.country === undefined ? "" : `,${data.sys.country}`}`;
+  labelCity.textContent = `${data.name} ${
+    data.sys.country === undefined ? "" : `,${data.sys.country}`
+  }`;
   labelWeather.textContent = `${data.weather[0].main}`;
   labelTemp.textContent = `${Math.round(data.main.temp) - 273}°c`;
-  labelMinMax.textContent = `${Math.round(data.main.temp_min) - 273}°c / ${Math.round(data.main.temp_max) - 273
-    }°c`;
+  labelMinMax.textContent = `${Math.round(data.main.temp_min) - 273}°c / ${
+    Math.round(data.main.temp_max) - 273
+  }°c`;
 };
 
 // Fetching Weather by city
@@ -46,11 +53,9 @@ const getResults = async function (city) {
     const data = await res.json();
 
     renderResults(data);
-    
   } catch (err) {
-    alert("Can't Find Location")
+    alert("Can't Find Location");
   }
-
 };
 
 form.addEventListener("submit", function (e) {
