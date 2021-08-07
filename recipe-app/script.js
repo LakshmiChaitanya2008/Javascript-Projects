f6b36ae09adbde967ab0f2d09f10f11facb711ab
LakshmiChaitanya2008 <72391391+LakshmiChaitanya2008@users.noreply.github.com>
2021-08-07 18:01:59 +0530

    Add files via upload

--- recipe-app/script.js
+++ recipe-app/script.js
@@ -1,8 +1,29 @@
-import { APP_ID, APP_KEY } from "./config.js";
+const APP_ID = "fe4a26f4";
+const APP_KEY = "31eac63f01ad8e529453651f9fdd87d4";
 
 const input = document.querySelector("input");
 const form = document.querySelector("form");
 const recipesContainer = document.querySelector(".recipes");
+
+window.addEventListener("load", function () {
+  const arr = ["egg", "pizza", "burger", "india", "US", "australia", "china"];
+  const random = arr[Math.trunc(Math.random() * arr.length + 1)];
+  getResults(random);
+});
+
+const renderSpinner = function () {
+  recipesContainer.insertAdjacentHTML(
+    `afterbegin`,
+    `<div class="spinner">
+        <div class="rect1"></div>
+        <div class="rect2"></div>
+        <div class="rect3"></div>
+        <div class="rect4"></div>
+        <div class="rect5"></div>
+     </div>
+`
+  );
+};
 
 const renderRecipes = function (data) {
   const html = `
@@ -16,6 +37,7 @@ const renderRecipes = function (data) {
 };
 
 const getResults = async function (foodItem) {
+  renderSpinner();
   const res =
     await fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=${foodItem}&app_id=${APP_ID}&app_key=${APP_KEY}
   `);
